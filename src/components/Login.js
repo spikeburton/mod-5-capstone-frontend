52430e7263ed4a7ea02d49ba06851431861739aa
spikeburton <ralphburton3@gmail.com>
2019-04-25 14:47:05 -0400

    finish styling login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,45 +1,105 @@
 import React, { Component } from "react";
-import { Form, Button, Segment, Message } from "semantic-ui-react";
+import {
+  Form,
+  Button,
+  Segment,
+  Message,
+  GridColumn,
+  Grid
+} from "semantic-ui-react";
+import { API } from "../data";
 
 class Login extends Component {
   state = {
     username: "",
     password: ""
-  }
+  };
 
   handleChange = e => {
     this.setState({
       [e.target.name]: e.target.value
-    })
-  }
+    });
+  };
 
-  handleSubmit = () => {
-    console.log(this.state)
-  }
+  handleSubmit = e => {
+    e.preventDefault();
+    delete this.state.errors;
+
+    fetch(`${API}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({ user: this.state })
+    })
+      .then(response => response.json())
+      .then(payload => {
+        if (payload.errors) {
+          this.setState({ errors: payload.errors });
+        } else {
+          localStorage.setItem("token", payload.jwt);
+          this.props.history.push("/");
+        }
+      });
+
+    e.target.reset();
+  };
+
+  handleReset = () => {
+    this.setState({
+      username: "",
+      password: ""
+    });
+  };
 
   render() {
     return (
       <Segment id="login-container">
-        <Message attached header="Welcome" content="Please Log In" />
-        <Form id="login-form" className="attached fluid segment" onSubmit={this.handleSubmit}>
+        <Message id="login-message" attached header="Welcome" content="Please Log In" />
+        <Form
+          className="attached fluid segment"
+          onSubmit={this.handleSubmit}
+          onReset={this.handleReset}
+        >
           <Form.Input
             fluid
+            icon="user"
+            iconPosition="left"
             label="Username"
             placeholder="Username"
             type="text"
             name="username"
+            value={this.state.username}
             onChange={this.handleChange}
           />
           <Form.Input
             fluid
+            icon="lock"
+            iconPosition="left"
             label="Password"
             placeholder="Password"
             type="password"
             name="password"
+            value={this.state.password}
             onChange={this.handleChange}
           />
-          <Button type="submit">Login</Button>
+          <Grid textAlign="center">
+            <GridColumn>
+              <Button color="black" type="submit">
+                Login
+              </Button>
+            </GridColumn>
+          </Grid>
         </Form>
+        {this.state.errors ? (
+          <Message
+            attached
+            error
+            header="There were errors with your submission:"
+            list={this.state.errors}
+          />
+        ) : null}
       </Segment>
     );
   }
