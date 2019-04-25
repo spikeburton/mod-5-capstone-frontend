8430ec7aadf93a268d1ba2c7e404d8848adbf8a0
spikeburton <ralphburton3@gmail.com>
2019-04-25 11:39:27 -0400

    add react router and login component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
 
 class App extends Component {
   render() {
     return (
-      <div></div>
-    )
+      <div id="app-container">
+        <Router>
+          <Switch>
+            <Route exact path={"/login"} component={Login} />
+          </Switch>
+        </Router>
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
