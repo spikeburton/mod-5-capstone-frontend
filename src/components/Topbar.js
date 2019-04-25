54b02b11ae9fb5cb3f53e0f313bed412fe80bb81
spikeburton <ralphburton3@gmail.com>
2019-04-25 17:46:25 -0400

    add signup button to topbar

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Grid } from "semantic-ui-react";
+import SignupButton from "./SignupButton";
 
 const Topbar = props => {
   return (
     <Grid relaxed stackable columns={2}>
       <Grid.Column>
         <div id="topbar">
-          <h1>Choose a Scenic Drive.</h1>
+          <h1>Take a Scenic Drive.</h1>
+          <p>Choose from a list of scenic byways in your state, or add your own route. See what others have been upto.</p>
+          <SignupButton />
         </div>
       </Grid.Column>
       <Grid.Column>
