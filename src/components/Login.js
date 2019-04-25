8c8d2281353ae3d98bb44fdffa861baeb8d26cd3
spikeburton <ralphburton3@gmail.com>
2019-04-25 11:51:23 -0400

    add login inputs

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
+import { Form, Button } from 'semantic-ui-react'
 
 class Login extends Component {
   render() {
     return (
-      <div>Hello</div>
+      // <div>Hello</div>
+      <Form className="attached fluid segment">
+        <Form.Input fluid label='Username' placeholder="Username" type="text" />
+        <Form.Input fluid label='Password' placeholder="Password" type="text" />
+        <Button>Submit</Button>
+      </Form>
     )
   }
 }
