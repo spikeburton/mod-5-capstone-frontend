a40a75b80d9fb5a8b392546775dc4c0410d8ae91
spikeburton <ralphburton3@gmail.com>
2019-04-26 10:15:59 -0400

    grab route data from API

--- src/components/ViewModal.js
+++ src/components/ViewModal.js
@@ -5,6 +5,8 @@ import CloseButton from "./CloseButton";
 import mapboxgl, { Map } from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
+import { DIRECTIONS_API } from "../data";
+
 mapboxgl.accessToken =
   "pk.eyJ1Ijoic3Bpa2VidXJ0b24iLCJhIjoiY2p0MDhsbmpuMDEwajQzbWp4Mnd4a2hneiJ9.hejKLROWCOdlcjV6W67qHw";
 
@@ -16,7 +18,7 @@ class ViewModal extends Component {
       latA: viewed.bound_a_lat,
       lngB: viewed.bound_b_lng,
       latB: viewed.bound_b_lat
-    }
+    };
 
     this.map = new Map({
       container: this.mapContainer,
@@ -24,7 +26,15 @@ class ViewModal extends Component {
       center: [bounds.lngA, bounds.latA],
       zoom: 7
     });
+
+    fetch(`${DIRECTIONS_API}/${bounds.lngA},${bounds.latA};${bounds.lngB},${bounds.latB}?geometries=geojson&steps=true&access_token=${mapboxgl.accessToken}`)
+    .then(response => response.json())
+    .then(this.drawRoute)
   };
+
+  drawRoute = payload => {
+    console.log(payload)
+  }
 
   render() {
     const { viewed } = this.props;
