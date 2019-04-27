d4957dad427a20e4e3b07a808868c215b99d094a
spikeburton <ralphburton3@gmail.com>
2019-04-27 10:31:15 -0400

    return loading and data in state

--- src/reducers/drivesReducer.js
+++ src/reducers/drivesReducer.js
@@ -1,7 +1,7 @@
-export const drivesReducer = (state = { data: [] }, action) => {
+export const drivesReducer = (state = { loading: false, data: [] }, action) => {
   switch (action.type) {
     case "LOADING_DRIVES":
-      return { loading: true };
+      return { ...state, loading: true };
     case "FETCH_DRIVES":
       return { loading: false, data: action.payload };
     default:
