6b04732afa7d2d5095f59f307f9a4cb9fdb36f97
spikeburton <ralphburton3@gmail.com>
2019-04-27 17:37:02 -0400

    add container for map modal

---
+++ src/containers/ViewModalContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class ViewModalContainer extends Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default connect()(ViewModalContainer)
