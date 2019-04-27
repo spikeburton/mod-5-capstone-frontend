f87b85b0f0b2b579e85822634ce86407d140edaf
spikeburton <ralphburton3@gmail.com>
2019-04-27 18:27:28 -0400

    refactor as functional component

--- src/components/DirectionListItem.js
+++ src/components/DirectionListItem.js
@@ -1,19 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import { List } from "semantic-ui-react";
 
-class DirectionListItem extends Component {
-  render() {
-    // console.log(this.props.maneuver)
-    return (
-      <List.Item>
-        <List.Icon name="marker" />
-        <List.Content>
-          {/* <List.Header>{this.props.maneuver.type}</List.Header> */}
-          <List.Description>{this.props.maneuver.instruction}</List.Description>
-        </List.Content>
-      </List.Item>
-    );
-  }
-}
+const DirectionListItem = props => {
+  // console.log(this.props.maneuver)
+  return (
+    <List.Item>
+      <List.Icon name="marker" />
+      <List.Content>
+        {/* <List.Header>{this.props.maneuver.type}</List.Header> */}
+        <List.Description>{props.maneuver.instruction}</List.Description>
+      </List.Content>
+    </List.Item>
+  );
+};
 
 export default DirectionListItem;
