1efd715f9c500e8f584bd74178bc5da892c46f96
spikeburton <ralphburton3@gmail.com>
2019-04-27 12:16:04 -0400

    add action types for loading/fetching directions

--- src/reducers/mapReducer.js
+++ src/reducers/mapReducer.js
@@ -1,12 +1,16 @@
 export const mapReducer = (
-  state = { modalOpen: false, current: null },
+  state = { modalOpen: false, current: null, loading: false, directions: [] },
   action
 ) => {
   switch (action.type) {
     case "OPEN_MODAL":
       return { ...state, modalOpen: true, current: action.current };
     case "CLOSE_MODAL":
-      return { ...state, modalOpen: false, current: null }
+      return { ...state, modalOpen: false, current: null, directions: [] }
+    case "LOADING_DIRECTIONS":
+      return { ...state, loading: true }
+    case "FETCH_DIRECTIONS":
+      return { ...state, loading: false, directions: action.payload }
     default:
       return state;
   }
