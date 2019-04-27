420d425d6a399fd2ce3f1407c213ce0d06b3a70f
spikeburton <ralphburton3@gmail.com>
2019-04-27 10:29:34 -0400

    begin redux refactor

--- src/containers/MainContentContainer.js
+++ src/containers/MainContentContainer.js
@@ -1,49 +1,70 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
 import SubMenu from "../components/SubMenu";
 import MainContent from "../components/MainContent";
-import { API } from "../data";
+// import { API } from "../data";
 import ViewModal from "../components/ViewModal";
 
+import { fetchDrives } from "../actions/driveActions";
+
 class MainContentContainer extends Component {
-  state = {
-    drives: [],
-    modalOpen: false,
-    viewed: null
-  };
+  // state = {
+  //   drives: [],
+  //   modalOpen: false,
+  //   viewed: null
+  // };
 
-  handleView = id => {
-    this.setState({
-      modalOpen: true,
-      viewed: this.state.drives.find(drive => drive.id === id)
-    });
-  };
+  // handleView = id => {
+  //   this.setState({
+  //     modalOpen: true,
+  //     viewed: this.state.drives.find(drive => drive.id === id)
+  //   });
+  // };
 
-  closeModal = () => {
-    this.setState({
-      modalOpen: false,
-      viewed: null
-    });
-  };
+  // closeModal = () => {
+  //   this.setState({
+  //     modalOpen: false,
+  //     viewed: null
+  //   });
+  // };
 
   componentDidMount() {
-    fetch(`${API}/drives`)
-      .then(response => response.json())
-      .then(payload => this.setState({ drives: payload }));
+    // fetch(`${API}/drives`)
+    //   .then(response => response.json())
+    //   .then(payload => this.setState({ drives: payload }));
+    this.props.fetchDrives();
   }
 
   render() {
+    console.log(this.props)
     return (
       <div>
         <SubMenu />
-        <MainContent drives={this.state.drives} handleView={this.handleView} />
-        <ViewModal
+        {/* <MainContent drives={this.props.drives} handleView={this.handleView} /> */}
+        {/* <ViewModal
           modalOpen={this.state.modalOpen}
           viewed={this.state.viewed}
           closeModal={this.closeModal}
-        />
+        /> */}
       </div>
     );
   }
 }
 
-export default MainContentContainer;
+const mapStateToProps = state => {
+  return {
+    drives: state.drives.data
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchDrives: () => fetchDrives()(dispatch)
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MainContentContainer);
