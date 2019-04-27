f3aa78c76ab7a3fd64aebe1ae1fec7de01e1710e
spikeburton <ralphburton3@gmail.com>
2019-04-27 09:46:21 -0400

    add root reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,6 @@
-import {combineReducers} from 'redux'
+import { combineReducers } from "redux";
 import { drivesReducer } from "./drivesReducer";
 
 export const rootReducer = combineReducers({
   drives: drivesReducer
-})
+});
