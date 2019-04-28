7452f88bc4ba33f0fb5c0899803038c4f2239747
spikeburton <ralphburton3@gmail.com>
2019-04-28 13:14:00 -0400

    add distance to directions list

--- src/components/DirectionListItem.js
+++ src/components/DirectionListItem.js
@@ -3,23 +3,24 @@ import { List, Image } from "semantic-ui-react";
 
 class DirectionListItem extends Component {
   render() {
-    // console.log(this.props.distance)
-
     // Parse file name from object properties
+    // FORMAT: direction_type_modifier
     let { type, modifier } = this.props.maneuver;
     type = type ? `_${type.split(" ").join("_")}` : "";
     modifier = modifier ? `_${modifier.split(" ").join("_")}` : "";
     const icon = `direction${type}${modifier}`;
-    console.log(icon);
+
+    let { distance } = this.props;
+    // Convert meters to kilometers
+    // ROUND to 2 decimal places: round((distance / 1000) * 100) / 100
+    distance = (distance >= 1000) ? `${Math.round(distance / 10) / 100} km` : `${distance} m`
 
     return (
       <List.Item>
-        {/* <List.Icon name={icon} /> */}
         <Image avatar src={require(`../icons/png/light/${icon}.png`)} />
         <List.Content>
-          {/* <List.Header>{this.props.maneuver.type}</List.Header> */}
           <List.Header>{this.props.maneuver.instruction}</List.Header>
-          <List.Description>distance</List.Description>
+          <List.Description>{distance}</List.Description>
         </List.Content>
       </List.Item>
     );
