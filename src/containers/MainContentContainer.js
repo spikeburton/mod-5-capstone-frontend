cfac51115fd7e047f3d4c3c75202db65eb3be2ef
spikeburton <ralphburton3@gmail.com>
2019-04-29 17:13:46 -0400

    refactor methods to save or remove a drive from favorites using redux

--- src/containers/MainContentContainer.js
+++ src/containers/MainContentContainer.js
@@ -6,68 +6,17 @@ import MainContent from "../components/MainContent";
 import ViewModalContainer from "./ViewModalContainer";
 
 import { fetchDrives } from "../actions/driveActions";
-import { fetchFavorites } from "../actions/favoriteActions";
+import {
+  fetchFavorites,
+  addFavorite,
+  removeFavorite
+} from "../actions/favoriteActions";
 import { openModal } from "../actions/mapActions";
-import { API } from "../data";
 
 class MainContentContainer extends Component {
-  state = {
-    favorites: []
-  }
-
   componentDidMount() {
     this.props.fetchDrives();
     this.props.fetchFavorites();
-    // fetch(`${API}/profile`, {
-    //   method: "GET",
-    //   headers: {
-    //     Authorization: `Bearer ${localStorage.getItem("token")}`,
-    //   }
-    // })
-    // .then(response => response.json())
-    // .then(({ user }) => {
-    //   this.setState({
-    //     favorites: user.favorites
-    //   })
-    // })
-  }
-
-  handleSave = id => {
-    fetch(`${API}/favorites`, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ drive_id: id })
-    })
-    .then(response => response.json())
-    .then(payload => {
-      this.setState({
-        favorites: [
-          ...this.state.favorites,
-          payload
-        ]
-      })
-    })
-  }
-
-  handleUnsave = id => {
-    fetch(`${API}/favorites/${id}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`
-      }
-    })
-    .then(() => {
-      const i = this.state.favorites.findIndex(cur => cur.id === id)
-      this.setState({
-        favorites: [
-          ...this.state.favorites.slice(0, i),
-          ...this.state.favorites.slice(i + 1)
-        ]
-      })
-    })
   }
 
   render() {
@@ -77,9 +26,9 @@ class MainContentContainer extends Component {
         <MainContent
           drives={this.props.drives}
           handleView={this.props.openModal}
-          handleSave={this.handleSave}
-          handleUnsave={this.handleUnsave}
-          favorites={this.state.favorites}
+          handleSave={this.props.addFavorite}
+          handleUnsave={this.props.removeFavorite}
+          favorites={this.props.favorites}
         />
         <ViewModalContainer />
       </div>
@@ -98,6 +47,8 @@ const mapDispatchToProps = dispatch => {
   return {
     fetchDrives: () => fetchDrives()(dispatch),
     fetchFavorites: () => fetchFavorites()(dispatch),
+    addFavorite: id => addFavorite(id)(dispatch),
+    removeFavorite: id => removeFavorite(id)(dispatch),
     openModal: current => dispatch(openModal(current))
   };
 };
