c1a88c0aa3b6f145fe4d15459d72551271bc1e7e
spikeburton <ralphburton3@gmail.com>
2019-04-29 17:09:54 -0400

    add action to remove favorite from database

--- src/actions/favoriteActions.js
+++ src/actions/favoriteActions.js
@@ -2,14 +2,45 @@ import { API } from "../data";
 
 export const fetchFavorites = () => {
   return dispatch => {
-    dispatch({ type: "LOADING_FAVORITES" })
+    dispatch({ type: "LOADING_FAVORITES" });
     return fetch(`${API}/profile`, {
       method: "GET",
       headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        Authorization: `Bearer ${localStorage.getItem("token")}`
       }
     })
-    .then(response => response.json())
-    .then(payload => dispatch({ type: "FETCH_FAVORITES", payload: payload.user.favorites }))
+      .then(response => response.json())
+      .then(payload =>
+        dispatch({ type: "FETCH_FAVORITES", payload: payload.user.favorites })
+      );
+  };
+};
+
+export const addFavorite = id => {
+  return dispatch => {
+    dispatch({ type: "LOADING_FAVORITES" });
+    return fetch(`${API}/favorites`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ drive_id: id })
+    })
+      .then(response => response.json())
+      .then(payload => dispatch({ type: "ADD_FAVORITE", payload }));
+  };
+};
+
+export const removeFavorite = id => {
+  return dispatch => {
+    dispatch({ type: "LOADING_FAVORITES" })
+    return fetch(`${API}/favorites/${id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      }
+    })
+    .then(() => dispatch({ type: "REMOVE_FAVORITE", id }))
   }
 }
