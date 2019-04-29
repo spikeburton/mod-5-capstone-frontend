ddeb38f20e2dec53973293b982ed3c76576d0ba9
spikeburton <ralphburton3@gmail.com>
2019-04-29 16:37:13 -0400

    add favorites reducer to root reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { drivesReducer } from "./drivesReducer";
 import { mapReducer } from "./mapReducer";
+import { favoritesReducer } from "./favoritesReducer";
 
 export const rootReducer = combineReducers({
   drives: drivesReducer,
+  favorites: favoritesReducer,
   map: mapReducer
 });
