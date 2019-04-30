b0a36f2b2c42f5d6a36c065105d7630b6e7e2ec1
spikeburton <ralphburton3@gmail.com>
2019-04-30 15:31:03 -0400

    add form fields for user data

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react'
-import { API } from '../data';
+import React, { Component, Fragment } from "react";
+import { API } from "../data";
+import { Segment, Grid, Form } from "semantic-ui-react";
+import Navbar from "./Navbar";
 
 class Settings extends Component {
   state = {
     first_name: "",
     last_name: "",
     email: ""
-  }
+  };
 
   componentDidMount() {
     fetch(`${API}/profile`, {
@@ -16,21 +18,65 @@ class Settings extends Component {
         Accept: "application/json"
       }
     })
-    .then(response => response.json())
-    .then(({user}) => {
-      this.setState({
-        first_name: user.first_name,
-        last_name: user.last_name,
-        email: user.email
-      })
+      .then(response => response.json())
+      .then(({ user }) => {
+        this.setState({
+          first_name: user.first_name,
+          last_name: user.last_name,
+          email: user.email
+        });
+      });
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
     })
   }
 
   render() {
     return (
-      <div></div>
-    )
+      <Fragment>
+        <Navbar active="settings" />
+        <Segment>
+          <Grid columns="2" centered verticalAlign="middle">
+            <Grid.Column>
+              <Form>
+                <Form.Input
+                  fluid
+                  type="text"
+                  label="First Name"
+                  placeholder="First Name"
+                  name="first_name"
+                  value={this.state.first_name}
+                  onChange={this.handleChange}
+                />
+                <Form.Input
+                  fluid
+                  type="text"
+                  label="Last Name"
+                  placeholder="Last Name"
+                  name="last_name"
+                  value={this.state.last_name}
+                  onChange={this.handleChange}
+                />
+                <Form.Input
+                  fluid
+                  type="text"
+                  label="Email"
+                  placeholder="Email"
+                  name="email"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                />
+              </Form>
+            </Grid.Column>
+            <Grid.Column>Hello</Grid.Column>
+          </Grid>
+        </Segment>
+      </Fragment>
+    );
   }
 }
 
-export default Settings
+export default Settings;
