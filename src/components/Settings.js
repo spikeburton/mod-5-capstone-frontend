e331b8c98ed9c5190684108a508d13b9b51ff781
spikeburton <ralphburton3@gmail.com>
2019-04-30 14:55:22 -0400

    add settings component

---
+++ src/components/Settings.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Settings extends Component {
+  componentDidMount() {
+    console.log("mounted")
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default Settings
