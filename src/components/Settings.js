643a69ab3a53d1f2c0254ffda4ea98107f8f4961
spikeburton <ralphburton3@gmail.com>
2019-04-30 15:17:10 -0400

    fetch user profile data from backend

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react'
+import { API } from '../data';
 
 class Settings extends Component {
+  state = {
+    first_name: "",
+    last_name: "",
+    email: ""
+  }
+
   componentDidMount() {
-    console.log("mounted")
+    fetch(`${API}/profile`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        Accept: "application/json"
+      }
+    })
+    .then(response => response.json())
+    .then(({user}) => {
+      this.setState({
+        first_name: user.first_name,
+        last_name: user.last_name,
+        email: user.email
+      })
+    })
   }
 
   render() {
