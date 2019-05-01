2cb1c7c3b6613b61f5321ef1d05913dfcb545b52
spikeburton <ralphburton3@gmail.com>
2019-04-30 21:53:44 -0400

    conditionally render content depending on user logged in

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,16 +1,21 @@
-import React, { Component } from 'react'
-import TopbarContainer from './TopbarContainer';
-import MainContentContainer from './MainContentContainer';
+import React, { Component } from "react";
+import TopbarContainer from "./TopbarContainer";
+import MainContentContainer from "./MainContentContainer";
+import NoUserContentContainer from "../components/NoUserContentContainer";
 
 class MainContainer extends Component {
   render() {
     return (
       <div>
         <TopbarContainer />
-        <MainContentContainer />
+        {localStorage.getItem("token") ? (
+          <MainContentContainer />
+        ) : (
+          <NoUserContentContainer />
+        )}
       </div>
-    )
+    );
   }
 }
 
-export default MainContainer
+export default MainContainer;
