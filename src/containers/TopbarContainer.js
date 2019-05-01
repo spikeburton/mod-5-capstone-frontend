14f4c389edb3c320a4be11178aecf759ee069d12
spikeburton <ralphburton3@gmail.com>
2019-05-01 18:19:06 -0400

    add random color to topbar when not logged in

--- src/containers/TopbarContainer.js
+++ src/containers/TopbarContainer.js
@@ -4,10 +4,30 @@ import Topbar from "../components/Topbar";
 
 class TopbarContainer extends Component {
   render() {
+    const colors = [
+      "#cddd3f",
+      "#e0ee5f",
+      "#e9e65b",
+      "#a9e49a",
+      "#8dd8b9",
+      "#2fcab5",
+      "#5dafd4",
+      "#6483d6",
+      "#6761c9",
+      "#ab6dcf"
+    ];
+
     return (
       <div>
-        <div id="topbar-container">
-        <Navbar active="main" />
+        <div
+          id="topbar-container"
+          style={{
+            background: localStorage.getItem("token")
+              ? "#aab4b2"
+              : colors[Math.floor(Math.random() * colors.length)]
+          }}
+        >
+          <Navbar active="main" />
           <Topbar />
         </div>
       </div>
