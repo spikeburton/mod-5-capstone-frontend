27050e9cb9be6a6e4b8ba43e031abf5b82e41273
spikeburton <ralphburton3@gmail.com>
2019-05-05 18:35:13 -0400

    add map

--- src/components/DriveCreation.js
+++ src/components/DriveCreation.js
@@ -1,11 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Container, Segment, Grid, Loader } from "semantic-ui-react";
 
 class DriveCreation extends Component {
   render() {
+    console.log(this.props.map);
     return (
-      <div></div>
-    )
+      <Container>
+        <Segment>
+          <Grid columns="2" stackable reversed="mobile">
+            <Grid.Column>
+              <p>Current longitude: {this.props.lng}</p>
+              <p>Current latitude: {this.props.lat}</p>
+            </Grid.Column>
+            <Grid.Column>
+              <div
+                id="create-map-container"
+                style={{ width: "100%", height: "400px", borderRadius: "7px" }}
+              />
+              {!(this.props.lat && this.props.lng) ? (
+                <Segment placeholder>
+                  <Loader size="medium" />
+                </Segment>
+              ) : null}
+            </Grid.Column>
+          </Grid>
+        </Segment>
+      </Container>
+    );
   }
 }
 
-export default DriveCreation
+export default DriveCreation;
--- src/containers/DriveCreationContainer.js
+++ src/containers/DriveCreationContainer.js
@@ -1,16 +1,63 @@
-import React, { Component } from 'react'
-import Navbar from '../components/Navbar';
-import DriveCreation from '../components/DriveCreation';
+import React, { Component } from "react";
+import Navbar from "../components/Navbar";
+import DriveCreation from "../components/DriveCreation";
+
+import { Map } from "mapbox-gl";
+
+import "mapbox-gl/dist/mapbox-gl.css";
 
 class DriveCreationContainer extends Component {
+  state = {
+    curLng: 0,
+    curLat: 0
+  };
+
+  getLocation = () => {
+    return new Promise((resolve, reject) => {
+      if (!navigator.geolocation) {
+        reject(new Error("Geoloation not supported"));
+      }
+
+      navigator.geolocation.getCurrentPosition(
+        pos => {
+          resolve(pos);
+        },
+        () => reject(new Error("Permission denied"))
+      );
+    });
+  };
+
+  componentDidMount() {
+    this.getLocation().then(pos => {
+      this.setState(
+        {
+          curLng: pos.coords.longitude,
+          curLat: pos.coords.latitude
+        },
+        () => {
+          this.map = new Map({
+            style: "mapbox://styles/mapbox/navigation-guidance-day-v2",
+            container: "create-map-container",
+            center: [this.state.curLng, this.state.curLat],
+            zoom: 8
+          });
+        }
+      );
+    });
+  }
+
   render() {
     return (
       <div>
         <Navbar />
-        <DriveCreation />
+        <DriveCreation
+          lng={this.state.curLng}
+          lat={this.state.curLat}
+          map={this.map}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default DriveCreationContainer
+export default DriveCreationContainer;
