efcedf3340588e094d7e5e600de3ae447596bb4c
spikeburton <ralphburton3@gmail.com>
2019-05-09 22:37:40 -0400

    Updates

---
+++ precache-manifest.8ba1b28870bea5224a0896f1e932e3a2.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c413f5c717ea29170034a86f9f58db78",
+    "url": "/mod-5-capstone-frontend/index.html"
+  },
+  {
+    "revision": "f8b0abf7b812fbf1df2a",
+    "url": "/mod-5-capstone-frontend/static/css/2.7dfe1aab.chunk.css"
+  },
+  {
+    "revision": "1819f4543f8198261018",
+    "url": "/mod-5-capstone-frontend/static/css/main.8d49cfe8.chunk.css"
+  },
+  {
+    "revision": "f8b0abf7b812fbf1df2a",
+    "url": "/mod-5-capstone-frontend/static/js/2.2c63dbf1.chunk.js"
+  },
+  {
+    "revision": "1819f4543f8198261018",
+    "url": "/mod-5-capstone-frontend/static/js/main.3b341842.chunk.js"
+  },
+  {
+    "revision": "04d625a462487cd24318",
+    "url": "/mod-5-capstone-frontend/static/js/runtime~main.d6d2ff85.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/mod-5-capstone-frontend/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/mod-5-capstone-frontend/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/mod-5-capstone-frontend/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/mod-5-capstone-frontend/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/mod-5-capstone-frontend/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "1b23ebd3995f0fd7ac2dd517e9aed8de",
+    "url": "/mod-5-capstone-frontend/static/media/default.1b23ebd3.jpeg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/mod-5-capstone-frontend/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/mod-5-capstone-frontend/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/mod-5-capstone-frontend/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/mod-5-capstone-frontend/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/mod-5-capstone-frontend/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/mod-5-capstone-frontend/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/mod-5-capstone-frontend/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/mod-5-capstone-frontend/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/mod-5-capstone-frontend/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/mod-5-capstone-frontend/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/mod-5-capstone-frontend/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "2ada9e6143d0e8bc807a9ab50a90e1d8",
+    "url": "/mod-5-capstone-frontend/static/media/tesla2.2ada9e61.png"
+  },
+  {
+    "revision": "b22864f05aade15ab80ff016d9652177",
+    "url": "/mod-5-capstone-frontend/static/media/woman-pointing.b22864f0.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
+
+importScripts(
+  "/mod-5-capstone-frontend/precache-manifest.8ba1b28870bea5224a0896f1e932e3a2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mod-5-capstone-frontend/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
